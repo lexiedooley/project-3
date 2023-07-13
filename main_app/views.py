1da7ea23e12977d4d295fdd3c930c28a87960baa
Stelusno <stelusno@gmail.com>
2023-07-13 12:35:16 -0400

    Update Delete Details and admin updated

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from django.views.generic.edit import UpdateView, DeleteView
+from .models import Movie
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+
+class MovieCreate(UpdateView):
+    model = Movie
+    fields = ['year', 'genre', 'rating', 'description']
+
+class MovieCreate(DeleteView):
+    model = Movie
+    success_url = '/movies'
+
+
